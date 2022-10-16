87fcde865cb4b224464b508b7813b8d584490e35
nfphys <nfphys@gmail.com>
2022-10-16 15:35:09 +0900

    ClientモデルのIDと認証機能を実装

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,32 @@
 class Client < ApplicationRecord
+  before_create :set_uuid 
+  validates :secret_digest, presence: true
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? 
+      BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_secret
+    SecureRandom.hex(64)
+  end
+
+  def initialize(secret:)
+    super(secret_digest: self.class.digest(secret))
+  end
+
+  def authenticated?(secret)
+    BCrypt::Password.new(secret_digest).is_password?(secret)
+  end
+
+  private
+
+    def set_uuid 
+      while self.id.blank? || self.class.find_by(id: self.id).present? do 
+        self.id = SecureRandom.uuid
+      end
+    end
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "有効な状態をもつ" do 
+    secret = Client.new_secret
+    client = Client.new(secret: secret)
+    expect(client).to be_valid
+  end
+
+  it "secretが正しければ認証される" do
+    secret = Client.new_secret
+    client = Client.new(secret: secret)
+    expect(client.authenticated?(secret)).to be_truthy
+  end
+
+  it "secretが間違っていれば認証されない" do 
+    secret = Client.new_secret
+    client = Client.new(secret: secret)
+    expect(client.authenticated?("wrong secret")).to be_falsy
+  end
 end
