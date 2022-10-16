b7215b41cfd9b4e724098b92219b079830c57ac0
nfphys <nfphys@gmail.com>
2022-10-16 15:27:32 +0900

    controller_specs: falseを追加

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module RailsOauthServer
         fixtures: false,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false
+        routing_specs: false,
+        controller_specs: false
     end
 
     # Settings in config/environments/* take precedence over those specified here.
