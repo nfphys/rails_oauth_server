234e35240a83a281a6e7eef4560da56e537cc55f
nfphys <nfphys@gmail.com>
2022-10-16 16:32:02 +0900

    Clientモデルのカラムを修正

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
-  before_create :set_uuid 
-  validates :secret_digest, presence: true
+  has_many :redirect_uris, class_name: "ClientRedirectUri", foreign_key: "client_id"
+  validates :client_secret_digest, presence: true
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? 
@@ -10,23 +10,22 @@ class Client < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
+  def self.new_id
+    SecureRandom.uuid
+  end
+
   def self.new_secret
     SecureRandom.hex(64)
   end
 
-  def initialize(secret:)
-    super(secret_digest: self.class.digest(secret))
+  def initialize(client_id:, client_secret:)
+    super(
+      client_id: client_id,
+      client_secret_digest: self.class.digest(client_secret),
+    )
   end
 
-  def authenticated?(secret)
-    BCrypt::Password.new(secret_digest) == secret
+  def authenticated?(client_secret)
+    BCrypt::Password.new(client_secret_digest) == client_secret
   end
-
-  private
-
-    def set_uuid 
-      while self.id.blank? || self.class.find_by(id: self.id).present? do 
-        self.id = SecureRandom.uuid
-      end
-    end
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -2,20 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Client, type: :model do
   it "有効な状態をもつ" do 
-    secret = Client.new_secret
-    client = Client.new(secret: secret)
+    client_id = Client.new_id
+    client_secret = Client.new_secret
+    client = Client.new(
+      client_id: client_id,
+      client_secret: client_secret
+    )
     expect(client).to be_valid
   end
 
   it "secretが正しければ認証される" do
-    secret = Client.new_secret
-    client = Client.new(secret: secret)
-    expect(client.authenticated?(secret)).to be_truthy
+    client_id = Client.new_id
+    client_secret = Client.new_secret
+    client = Client.new(
+      client_id: client_id,
+      client_secret: client_secret
+    )
+    expect(client.authenticated?(client_secret)).to be_truthy
   end
 
   it "secretが間違っていれば認証されない" do 
-    secret = Client.new_secret
-    client = Client.new(secret: secret)
+    client_id = Client.new_id
+    client_secret = Client.new_secret
+    client = Client.new(
+      client_id: client_id,
+      client_secret: client_secret
+    )
     expect(client.authenticated?("wrong secret")).to be_falsy
   end
 end
