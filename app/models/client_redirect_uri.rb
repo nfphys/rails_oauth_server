659c1af40cf80dbea258bef2131dde891f601d10
nfphys <nfphys@gmail.com>
2022-10-17 00:48:23 +0900

    validationとspecを追加

--- app/models/client_redirect_uri.rb
+++ app/models/client_redirect_uri.rb
@@ -1,3 +1,4 @@
 class ClientRedirectUri < ApplicationRecord
   belongs_to :client
+  validates :uri, presence: :true
 end
--- spec/models/client_redirect_uri_spec.rb
+++ spec/models/client_redirect_uri_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe ClientRedirectUri, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "有効な状態を持つ" do
+    client = Client.create(secret: Client.new_secret)
+    redirect_uri = client.redirect_uris.new(uri: "http://example.com")
+    expect(redirect_uri).to be_valid
+  end
+
+  it "uriが空文字なら無効である" do
+    client = Client.create(secret: Client.new_secret)
+    redirect_uri = client.redirect_uris.new(uri: "   ")
+    expect(redirect_uri).to_not be_valid
+  end
+
+  it "client_idが指定されていなければ無効である" do
+    redirect_uri = ClientRedirectUri.new(uri: "http://example.com")
+    expect(redirect_uri).to_not be_valid
+  end
 end
