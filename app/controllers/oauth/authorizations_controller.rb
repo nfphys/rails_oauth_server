3fd45e184ea1a0e3b15621770f2c74d5d327475d
nfphys <nfphys@gmail.com>
2022-10-16 17:50:34 +0900

    authorization_codeのフォームを作成

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -2,5 +2,17 @@ class Oauth::AuthorizationsController < ApplicationController
   skip_before_action :authenticate
 
   def new
+    @client = Client.find_by(uuid: params[:client_id])
+    if !@client 
+      redirect_to root_path
+      return
+    end
+
+    @authorization_code = AuthorizationCode.new(
+      redirect_uri: params[:redirect_uri],
+      state: params[:state],
+      client_id: @client.id,
+      user_id: session[:user_id],
+    )
   end
 end
