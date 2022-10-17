9c82c6c54c1e87413991292aa9c2d8864b6523e3
nfphys <nfphys@gmail.com>
2022-10-17 18:02:40 +0900

    authorization_codeモデルのspecを追加

--- spec/models/authorization_code_spec.rb
+++ spec/models/authorization_code_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AuthorizationCode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @client = Client.create(secret: Client.new_secret)
+    @user = User.create(
+      email: "foo@example.com",
+      password: "password",
+      password_confirmation: "password",
+    )
+  end
+
+  it "有効な状態を持つ" do
+    authorization_code = @client.authorization_codes.new(
+      code: AuthorizationCode.new_code,
+      redirect_uri: "http://example.com/oauth/callback",
+      state: SecureRandom.urlsafe_base64,
+      user_id: @user.id,
+    )
+    expect(authorization_code).to be_valid
+  end
 end
