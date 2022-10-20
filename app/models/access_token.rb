46de0d3eafb7973e52c346e05a48e2b7a4f122d7
nfphys <nfphys@gmail.com>
2022-10-20 17:52:32 +0900

    initializeメソッドを追加

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -3,4 +3,10 @@ class AccessToken < ApplicationRecord
   belongs_to :client
   validates :token, presence: true
   validates :expires_at, presence: true
+
+  def initialize(params)
+    params[:token] ||= SecureRandom.hex(64)
+    params[:expires_at] ||= Time.now + 1.minute
+    super(params)
+  end
 end
